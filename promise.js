6eb4cc9b3ae4fb8f178d4e70a2eaf247a0dea5d5
ferimustakim <ferimustakim@gmail.com>
2022-09-19 16:31:39 +0700

    yang ke berapa

--- promise.js
+++ promise.js
@@ -2,13 +2,24 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 //const promiseOutput = null;
-const promiseOutput = (emosi) => {
-  return new Promise((resolve, reject)=>{
+
+  const promiseOutput = async (emosi) => {
+    let Theater_IXX = await promiseTheaterIXX();
+    let Theater_VGC = await promiseTheaterVGC();
     
-    
+    return new Promise((resolve, reject)=>{
+      if(Theater_IXX !== undefined || Theater_VGC !== undefined){
+        let hitungIXX = Theater_IXX.filter((item)=>item.hasil == emosi).length;
+        let hitungVGC = Theater_VGC.filter((item)=>item.hasil == emosi).length;
+        
+        resolve(hitungIXX + hitungVGC);
+      }else{
+        reject("err");
+      }
     })
- 
-};
+  };
+    
+    
 
 
 module.exports = {
