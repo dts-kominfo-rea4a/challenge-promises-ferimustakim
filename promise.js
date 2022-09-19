fb86917ba3440c29aa313a40c8e7eaaa5675d51a
ferimustakim <ferimustakim@gmail.com>
2022-09-19 14:58:04 +0700

    ---

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+//const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject)=>{
+    
+    
+    })
+ 
+};
+
 
 module.exports = {
   promiseOutput,
